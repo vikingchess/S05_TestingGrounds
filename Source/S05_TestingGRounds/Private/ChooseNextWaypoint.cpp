36cad213951d87ee36c79aa94bdcd0f6be7fd287
DESKTOP-O8GP7PU\David <davidflom01@gmail.com>
2017-08-15 18:35:18 -0400

    end of video 222 working removed unused C++ classes

--- Source/S05_TestingGRounds/Private/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGRounds/Private/ChooseNextWaypoint.cpp
@@ -1,12 +1,33 @@
 // Copyright 2017 David Flom
 
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
+	
+
+	//Get Patrol route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute)) {return EBTNodeResult::Failed;}
+
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing control points"));
+		return EBTNodeResult::Failed;
+	}
+	//Set next way point
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index %i"), Index);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	//Cycle index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 	return EBTNodeResult::Succeeded;
 }
--- Source/S05_TestingGRounds/Private/PatrollingGuards.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2017 David Flom
-
-#include "PatrollingGuards.h"
-
-
-
-
