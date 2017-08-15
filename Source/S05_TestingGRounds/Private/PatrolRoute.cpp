cde4b43c4aff1b4ec9036d181736737822276f84
DESKTOP-O8GP7PU\David <davidflom01@gmail.com>
2017-08-15 14:44:44 -0400

    end of video of 219 working

---
+++ Source/S05_TestingGRounds/Private/PatrolRoute.cpp
@@ -0,0 +1,9 @@
+// Copyright 2017 David Flom
+
+#include "PatrolRoute.h"
+
+
+TArray<AActor*>UPatrolRoute::GetPatrolPoints() const
+{
+	return PatrolPoints;
+}
---
+++ Source/S05_TestingGRounds/Public/PatrolRoute.h
@@ -0,0 +1,23 @@
+// Copyright 2017 David Flom
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PatrolRoute.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class S05_TESTINGGROUNDS_API UPatrolRoute : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	TArray<AActor*> GetPatrolPoints() const;
+private:
+	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+	TArray<AActor*> PatrolPoints;
+
+		
+	
+};
